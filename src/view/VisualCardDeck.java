e63a56b51fb3794e13d3950d922df9c28335b5c6
hrstka94 <hrstka94@gmail.com>
2017-05-04 18:54:16 +0200

    Card Deck can be also 'Source' now.

--- src/view/VisualCardDeck.java
+++ src/view/VisualCardDeck.java
@@ -10,49 +10,44 @@ import src.controler.CommandMove;
 import src.controler.ICommand;
 import src.share.ICard;
 
-public class VisualCardDeck extends VisualAbstractDeck  {
-	
+public class VisualCardDeck extends VisualAbstractDeck {
 	ICardDeck deck;
 	private VisualCard card;
 			
 	public void paint() {
-		
 		ICard stackCard = deck.top();
 		VisualCard cardVis;
 		
 		if(stackCard != null){
 			cardVis = new VisualCard(VisualCard.VisualCardColor.visualColor(stackCard.color()), stackCard.value(), x, y );
-			
 			this.card = cardVis;
-			board.add(card);
-			
-		}else{
+			board.add(card);	
+		}
+		else{
 			cardVis = new VisualCard(VisualCard.VisualCardColor.NONE, 0, x, y );
-			
 			this.card = cardVis;
 			board.add(card);
 		}
-
 			
 		cardVis.addMouseListener(new MouseAdapter()  
 		{  
 		    public void mouseReleased(MouseEvent e)  
 		    {  
 				if(board.isMoveSourceSelected()){
-					ICardDeck source = board.getSelectedMoveSource();
-					
+					ICardDeck source = board.getSelectedMoveSource();	
 					ICommand command = new CommandMove(source, deck);
-					board.getCommandBuilder().execute(command);
-					
-					//deselect source
-					board.setSelectedMoveSource(null);
-					
-				}				    	
+					if(command.canExecute()){
+						board.getCommandBuilder().execute(command);
+						board.setSelectedMoveSource(null); //deselect source
+					}
+				}		
+				else{
+					board.setSelectedMoveSource(deck);
+				}		    	
 		    }  
-		}); 	
-		
-		
+		}); 		
 	}
+
 	public void setModel(ICardDeck stackModel) {
 		deck = stackModel;		
 	}
